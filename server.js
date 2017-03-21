3792b029bdb95b54214fc0e23362446e40cf0191
Alin <alinmitrea@gmail.com>
2017-03-21 15:52:50 +0100

    Adding quotes support

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
+var QUOTES_COLLECTION = "quotes";
 
 var app = express();
 app.use(bodyParser.json());
@@ -110,6 +111,131 @@ app.delete("/api/contacts/:id", function(req, res) {
       handleError(res, err.message, "Failed to delete contact");
     } else {
       res.status(200).json(req.params.id);
+    }
+  });
+});
+
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/api/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/contacts", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  }
+
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+/*  "/api/quotes"
+ *    GET: finds all quotes
+ *    POST: creates a new quote
+ */
+
+app.get("/api/quotes", function(req, res) {
+  db.collection(QUOTES_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get quotes.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/quotes", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  }
+
+  db.collection(QUOTES_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new quote.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+
+/*  "/api/quotes/:id"
+ *    GET: find quote by id
+ *    PUT: update quote by id
+ *    DELETE: deletes quote by id
+ */
+
+app.get("/api/quotes/:id", function(req, res) {
+  db.collection(QUOTES_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get quote");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+app.get("/api/quotes/quote_id/:quote_id", function(req, res) {
+  db.collection(QUOTES_COLLECTION).findOne({ quote_id: req.params.quote_id}, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get category");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+app.put("/api/quotes/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(QUOTES_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update quote");
+    } else {
+      updateDoc._id = req.params.id;
+      res.status(200).json(updateDoc);
+    }
+  });
+});
+
+app.delete("/api/quotes/:id", function(req, res) {
+  db.collection(QUOTES_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete quote");
+    } else {
+      res.status(200).json(req.params.id);
+    }
+  });
+});
+
+app.get("/api/quotes/:id", function(req, res) {
+  db.collection(QUOTES_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get quote");
+    } else {
+      res.status(200).json(doc);
     }
   });
 });
