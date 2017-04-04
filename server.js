9f6ebea3edc17784e04a7e76b63c9c38ec19402b
Alin <alinmitrea@gmail.com>
2017-04-04 17:41:11 +0200

    adding server limitation for get all quotes
    app.get("/api/quotes", function(req, res) {
      db.collection(QUOTES_COLLECTION).find({}).limit(2).toArray(function(err, docs) {
        if (err) {
          handleError(res, err.message, "Failed to get quotes.");
        } else {
          res.status(200).json(docs);
        }
      });
    });

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var db;
 //TODO: for local development add here the direct link do mongo db on mlab
 
 // Connect to the database before starting the application server.
-// process.env.MONGODB_URI = "mongodb://127.0.0.1:27017/admin";
+//process.env.MONGODB_URI = "mongodb://127.0.0.1:27017/admin";
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
@@ -156,7 +156,7 @@ app.post("/api/contacts", function(req, res) {
  */
 
 app.get("/api/quotes", function(req, res) {
-  db.collection(QUOTES_COLLECTION).find({}).toArray(function(err, docs) {
+  db.collection(QUOTES_COLLECTION).find({}).limit(2).toArray(function(err, docs) {
     if (err) {
       handleError(res, err.message, "Failed to get quotes.");
     } else {
