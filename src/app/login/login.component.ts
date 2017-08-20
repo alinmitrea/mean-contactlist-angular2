86e3c9fb1d084cc87b248e62e1e1b582689fbf01
alinmitrea <alinmitrea@gmail.com>
2017-08-20 18:33:07 +0200

    adding login validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {SharedService} from '../shared.service';
+import {Login} from "../models/login";
 
 @Component({
   selector: 'app-login',
@@ -8,18 +10,17 @@ import {SharedService} from '../shared.service';
 })
 export class LoginComponent implements OnInit {
   colorClass: string;
-  email: string;
+  model = new Login('', '','');
   constructor(private sharedService: SharedService) {
     this.colorClass = this.sharedService.backgroundColorClass + '-text';
   }
-  public isValidMailFormat() {
+  public isMailInvalid() {
     const EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
-    if (this.email !== '' && (this.email.length <= 5 || !EMAIL_REGEXP.test(this.email))) {
+    if (this.model.email !== '' && (this.model.email.length <= 5 || !EMAIL_REGEXP.test(this.model.email))) {
       return { 'Please provide a valid email': true };
     }
-
-    return null;
+    return false;
   }
   ngOnInit() {
   }
---
+++ src/app/models/login.ts
@@ -0,0 +1,9 @@
+export class Login {
+  constructor(
+    public _id: string,
+    public email: string,
+    public password: string
+  ) {
+
+  }
+}
